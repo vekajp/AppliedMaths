be1762c628f1c12b0a7d1d6278c338fa6e646435
vekajp <vekajp@gmail.com>
2022-12-20 00:09:40 +0300

    #LAB_1-10 state Готово

--- simplex.py
+++ simplex.py
@@ -13,28 +13,32 @@ class Simplex:
         self.feasible = False
 
     def optimize(self):
+        # Получаем матрицы из ввода
         A, b, c, minimize = Parser(self.constraints, self.objective).get_matrixes()
+        # Соединяем матрицы в табличку для вычислений
         tableau, base_vars = TableauUtils().get_initial_tableau(A, b, c, self.minimize)
+        # Оптимизируем
         try:
             tableau = self.simplex(tableau)
         except Exception as e:
             print(e.args)
             return None, []
 
+        # Убираем переменные, которые добавили для приведения задачи к каноническому ввиду
         tableau = TableauUtils().remove_redundant_variables(tableau, base_vars)
 
+        # Восстанавливаем оптимизируемую функцию в абличке
         tableau = TableauUtils().restore_tableau(tableau, base_vars, minimize)
 
-        tableau = self.simplex(tableau)
-
+        # Снова оптимизируем
         try:
             tableau = self.simplex(tableau)
         except Exception as e:
             print(e.args)
             return None, []
 
+        # Собираем ответ
         x = np.array([0] * len(c), dtype=float)
-
         for key in range(1, (len(tableau))):
             if tableau[key, 0] < len(c):
                 x[int(tableau[key, 0])] = tableau[key, 1]
@@ -58,6 +62,7 @@ class Simplex:
             tableau[row, 0] = column - 2
         return tableau
 
+    # Оптимальный элемент для ввода в базис
     def pivot_index(self, tableau):
         if self.minimize:
             column = tableau[0, 2:].tolist().index(np.amin(tableau[0, 2:])) + 2
@@ -73,6 +78,7 @@ class Simplex:
                     row = i
         return row, column
 
+    # Проверяем, можно ли еще оптимизировать
     def optimal(self, tableau):
         if not self.minimize:
             return np.max(tableau[0, 2:]) <= 0
